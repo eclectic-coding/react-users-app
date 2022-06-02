2b109fcfdf219ffec539c1eb8e9fd3ed9e0e3cc2
Chuck Smith <chuck.smith@opscompass.com>
2022-06-02 07:29:41 -0400

    Form validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,31 +4,38 @@ import Button from '../UI/Button';
 
 import classes from './AddUser.module.css';
 
-
 const AddUser = props => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value)
-  }
+    setEnteredUsername(event.target.value);
+  };
 
   const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value)
-  }
+    setEnteredAge(event.target.value);
+  };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" onChange={usernameChangeHandler}/>
+        <input type="text" id="username" onChange={usernameChangeHandler} value={enteredUsername} />
         <label htmlFor="age">age</label>
-        <input type="number" id="age" onChange={ageChangeHandler}/>
+        <input type="number" id="age" onChange={ageChangeHandler} value={enteredAge} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
